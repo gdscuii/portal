40d640911af29303ceb34e23e9a4fcf9f4ecae88
Albarra Erdanto <albarraerdanto@gmail.com>
2021-10-31 21:23:55 +0700

    add members section

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -80,10 +80,56 @@ async function renderMediumStories() {
     }
 }
 
+function memberTemplate(member) {
+    return (`
+        <li class="rounded-lg border-2 p-4 flex">
+            <div>
+            <img
+                src="${member.avatar_url ? member.avatar_url : `https://www.gravatar.com/avatar/${member.nama}?d=retro&f=y`}"
+                alt="${member.nama}"
+                class="w-10 h-10 object-cover rounded-full"
+            >
+            </div>
+            <div class="flex-1 ml-4">
+            <h3 class="font-bold">
+                ${member.nama}
+            </h3>
+            <p class="text-sm text-gray-500">
+                ${member.jurusan}, ${member.angkatan}
+            </p>
+            <div class="mt-1">
+                <a href="${member.github_url}" target="_blank" rel="noopener noreferrer">
+                    <i class="fab fa-github fa-lg text-black"></i>
+                </a>
+                <a href="${member.linkedin_url}" target="_blank" rel="noopener noreferrer">
+                    <i class="ml-1 fab fa-linkedin fa-lg text-blue-500"></i>
+                </a>
+            </div>
+            </div>
+        </li>
+    `)
+}
+
+async function getMembers() {
+    const res = await fetch('https://gdscuii.github.io/members/members.json');
+    const { members } = await res.json()
+    return members;
+}
+
+async function renderMembers() {
+    const membersDOM = document.getElementById('members-list');
+    try {
+        const members = await getMembers();
+        membersDOM.innerHTML = members.map(member => memberTemplate(member)).join('');
+    } catch (error) {
+        membersDOM.innerHTML = error
+    }
+}
 
 function renderHTML() {
     renderEvents();
     renderMediumStories();
+    renderMembers();
 }
 
 renderHTML();
